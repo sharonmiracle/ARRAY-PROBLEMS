73a706dba99dc8d0d07f4293c8d798b7b339ab73
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2024-03-09 11:25:58 +0530

    Create P4.cpp

---
+++ P4.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the no. of elements : ";
+    cin>>n;
+    int arr[n];
+    
+    // getting array values from the user
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    // printing array values
+    cout<<"The array elements are : ";
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    int x;
+    cout<<"\n\nEnter the value of x : ";
+    cin>>x;
+    int count = 0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i] <= x)
+        {
+            count+=1;
+        }
+    }
+    cout<<"\nThe Count of smaller elements is : "<<count;
+    return 0;
+}
