45a048da287d86ccf663c17c87e42a9e6f1020ca
SHARON EVANGELA MIRACLE A <116945951+sharonmiracle@users.noreply.github.com>
2024-08-06 14:00:59 +0530

    Create P16.c

---
+++ P16.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void reverse(int arr[],int start,int end)
+{
+    int temp;
+    while(start<end)
+    {
+        temp=arr[start];
+        arr[start]=arr[end];
+        arr[end]=temp;
+        start++;
+        end--;
+    }
+}
+void main()
+{
+    int size;
+    int rot_count;
+    int temp;
+    printf("Enter the no. of elements : ");
+    scanf("%d", &size);
+    int arr[size];
+    for(int ind=0; ind<size; scanf("%d",&arr[ind++]));
+    printf("Enter rotation count : ");
+    scanf("%d", &rot_count);
+    
+    reverse(arr,0,rot_count-1);
+    reverse(arr,rot_count,size-1);
+    reverse(arr,0,size-1);
+    
+    for(int ind=0; ind<size; printf("%d ", arr[ind++]));
+}
